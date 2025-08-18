5e9c572e405d582a337e0563ea62b92912f1f0f4
Negin228 <99446649+Negin228@users.noreply.github.com>
2025-08-18 16:27:04 -0700

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,20 @@
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 587
 
-tickers = ["AAPL", "TSLA", "AMZN","NFLX", "MSFT", "GOOG", "NVDA", "META"]
+tickers = ["AAPL", "TSLA", "AMZN","NFLX", "MSFT", "GOOG", "NVDA", "META", 
+"ABNB", "ADBE", "ADI", "ADP", "ADSK", "AEP", "AMD", "AMGN", "ANSS", "APA", "AVGO", "AXON", "AZN", "BIIB", "BKNG", "BKR", "CDNS", "CEG", "CHTR", "CMCSA", "COST", "CPRT", "CRWD", "CSCO", "CSGP", "CSX", "CTAS",
+  "DDOG", "DLR", "DXCM", "EA", "EXC", "FANG", "FAST", "FTNT", "GEHC", "GILD",
+  "HON", "IDXX", "INTC", "INTU", "ISRG", "KDP", "KHC", "KLAC",
+  "LIN", "LRCX", "LULU", "MAR", "MCHP", "MDLZ", "MELI", "MNST", "MRVL",
+  "MU", "NTES", "NXPI", "ODFL", "ON", "ORLY", "PANW",
+  "PAYX", "PCAR", "PEP", "PYPL", "QCOM", "REGN", "ROST", "ROKU", "ROP", "SBUX",
+  "SIRI", "SNPS", "SWKS", "TEAM", "TGT", "TMUS", "TXN",
+  "VRTX", "WBA", "WDAY", "XEL", "ZBRA", "ZM", "ZS"
+]
 
 RSI_OVERSOLD = 30
 RSI_OVERBOUGHT = 70
 MA_SHORT = 50
 MA_LONG = 200
+
 
