b0ecd06230aae5d386c4b33cdd8c0f07e0e7a79c
Negin228 <99446649+Negin228@users.noreply.github.com>
2025-08-18 17:14:20 -0700

    Update tickers.py

--- tickers.py
+++ tickers.py
@@ -1,7 +1,5 @@
 # Auto-generated ticker lists
 
-sp500_tickers = ["AAPL", "MSFT", "AMZN", ...]  # Full list from Wikipedia
-
-nasdaq100_tickers = ["AAPL", "MSFT", "NVDA", ...]  # Full list from Wikipedia
-
-all_tickers = ["AAPL", "MSFT", "AMZN", "NVDA", ...]  # Deduplicated merged list
+sp500_tickers = ["AAPL", "MSFT", "AMZN", "GOOGL", "TSLA", ...]
+nasdaq100_tickers = ["AAPL", "MSFT", "NVDA", "AMZN", "TSLA", ...]
+all_tickers = ["AAPL", "MSFT", "AMZN", "GOOGL", "TSLA", "NVDA", ...]  # deduplicated
