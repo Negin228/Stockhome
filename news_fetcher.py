89799973649d0047765a1b2e686dec5a1f354ddc
Negin228 <99446649+Negin228@users.noreply.github.com>
2025-09-17 17:18:02 -0700

    Update news_fetcher.py

--- news_fetcher.py
+++ news_fetcher.py
@@ -16,8 +16,8 @@ def fetch_news_ticker(ticker):
         news = finnhub_client.company_news(ticker, _from=from_date, to=to_date)
         summaries = []
         for article in news[:5]:
-            headline = article.get("title", "No Title")
-            url = article.get("link", "#")
+            headline = article.get("headline", "No Title")
+            url = article.get("url", "#")
             blob = TextBlob(headline)
             sentiment = blob.sentiment.polarity
             summaries.append({
