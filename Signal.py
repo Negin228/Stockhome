9f9c28471e07319b1e68d88b393c5901f6a9a53d
Negin228 <99446649+Negin228@users.noreply.github.com>
2025-09-18 16:20:07 -0700

    Update Signal.py

--- Signal.py
+++ Signal.py
@@ -446,6 +446,7 @@ def job(tickers):
         
         news_items = fetch_news_ticker(sym)
 
+        negative_news = [news for news in news_items if float(news.get('sentiment', 0)) < 0] if news_items else []
         use_news = None
         if negative_news:
             use_news = negative_news[0]
