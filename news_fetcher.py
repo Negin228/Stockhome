ef3a4361945294a5535e965480d2ca164a248b93
Negin228 <99446649+Negin228@users.noreply.github.com>
2025-09-17 23:21:42 -0700

    Update news_fetcher.py

--- news_fetcher.py
+++ news_fetcher.py
@@ -16,6 +16,8 @@ def fetch_news_ticker(ticker):
         news = finnhub_client.company_news(ticker, _from=from_date, to=to_date)
         summaries = []
         for article in news[:5]:
+            print(article.keys())
+            print(json.dumps(article, indent=2))  # show full available fields
             headline = article.get("headline", "No Title")
             url = article.get("url", "#")
             blob = TextBlob(headline)
