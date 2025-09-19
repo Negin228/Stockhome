557878648179c184f7da0c37ca4284dccc97bc10
Negin228 <99446649+Negin228@users.noreply.github.com>
2025-09-19 14:33:43 -0700

    Update news_fetcher.py

--- news_fetcher.py
+++ news_fetcher.py
@@ -63,7 +63,8 @@ def fetch_news_ticker(ticker):
 
                 article_text = get_article_text(url)
                 article_length = len(article_text.split())
-                logger.info(f"[NEWS] {ticker} | URL: {url} | Article length: {article_length} words")
+                print(f"[NEWS] {ticker} | URL: {url} | Article length: {article_length} words")
+
 
                 if article_text:
                     ai_summary = summarize_article_text(article_text)
