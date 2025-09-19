5a5bee4848c7daf3bdb329a7bbc714308613ce1d
Negin228 <99446649+Negin228@users.noreply.github.com>
2025-09-19 11:44:44 -0700

    Update Signal.py

--- Signal.py
+++ Signal.py
@@ -559,14 +559,13 @@ def main():
     all_buy_symbols = []
 
     #buysymbols, buy_alerts_web, buy_alerts_email, all_sell_alerts, failed = job(tickers)
-    all_buy_alerts.extend(buy_alerts_email)
+    #all_buy_alerts.extend(buy_alerts_email)
     #all_sell_alerts.extend(sell_alerts)
 
 
     while to_process and any(retry_counts[t] < MAX_TICKER_RETRIES for t in to_process):
         logger.info(f"Processing {len(to_process)} tickers...")
         buys, buy_alerts_web, buy_alerts_email, sells, fails = job(to_process)
-
         all_buy_alerts.extend(buy_alerts_email)
         all_sell_alerts.extend(sells)
         all_buy_symbols.extend(buys)
