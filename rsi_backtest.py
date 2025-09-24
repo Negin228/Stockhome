22eaca61fff7a2fb385bd27ce5f656fab101f516
Negin228 <99446649+Negin228@users.noreply.github.com>
2025-09-23 20:53:47 -0700

    Update rsi_backtest.py

--- rsi_backtest.py
+++ rsi_backtest.py
@@ -48,15 +48,11 @@ def fetch_cached_history(symbol, period="2y", interval="1d", max_cache_days=7):
 
 
 def calculate_indicators(df):
-    if df.empty or "Close" not in df.columns:
-        return df
-    close = df['Close']
-    # Make sure 'close' is a 1D Series
+    close = df["Close"]
     if isinstance(close, pd.DataFrame):
         close = close.squeeze()
-    df['rsi'] = RSIIndicator(close, window=14).rsi()
+    df["rsi"] = ta.momentum.RSIIndicator(close, window=14).rsi()
     return df
-
 
 cash = initial_cash
 holdings = {}  # {ticker: {'buy_price':..., 'buy_date':...}}
