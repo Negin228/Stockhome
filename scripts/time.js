36fc4b91acf964bf752749fbed1e23e3da712cbf
Negin228 <99446649+Negin228@users.noreply.github.com>
2025-10-04 18:10:02 -0700

    Update time.js

--- scripts/time.js
+++ scripts/time.js
@@ -1,6 +1,10 @@
-// time.js
-function updateTime() {
-    const now = new Date();
-    document.getElementById('time').innerText = now.toLocaleTimeString();
-}
-setInterval(updateTime, 1000);
+fetch('data/signals.json')
+  .then(response => response.json())
+  .then(data => {
+    allStocks = data.all || [];
+    filterStocks();
+    // Set last update
+    if (data.generated_at_pt) {
+      document.getElementById('last-update').innerText = "Last update: " + data.generated_at_pt;
+    }
+  });
