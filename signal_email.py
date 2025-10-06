16d1c0d7d63e95c0cce8409c7f320c304849cc75
Negin228 <99446649+Negin228@users.noreply.github.com>
2025-10-05 20:37:22 -0700

    Update signal_email.py

--- signal_email.py
+++ signal_email.py
@@ -249,7 +249,8 @@ def format_email_body_html(buy_alerts, sell_alerts):
         '<html><body style="font-family: Arial, sans-serif;">',
         f'<div style="margin-bottom:24px;">'
         f'<a href="https://stockhome.me" target="_blank">'
-        f'<img src="https://stockhome.me/assets/logo.png" alt="StockHome Logo" style="height:40px; max-width:220px; width:auto; vertical-align:middle;"></a>'
+        f'<img src="https://stockhome.me/assets/logo.png" alt="StockHome Logo" style="height:40px; max-width:220px; width:auto; vertical-align:middle;"></a>',
+        '<br>'
         f' <span style="font-size:2em;font-weight:bold;vertical-align:middle;">Trading Signals</span>'
         '</div>',
         f'<div><strong>Generated:</strong> {(datetime.datetime.now() - datetime.timedelta(hours=7)):%m-%d-%Y %H:%M} PT</div>',
