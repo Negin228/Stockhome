bcafdbdf838f0a65a57e43ec12d8d46a08fb8d77
Negin228 <99446649+Negin228@users.noreply.github.com>
2025-11-06 21:11:29 -0800

    Update API keys for paper trading

--- alpaca_options_trader.py
+++ alpaca_options_trader.py
@@ -7,9 +7,9 @@ from alpaca.trading.requests import OptionOrderRequest, OptionSymbolParams
 from alpaca.trading.enums import OptionOrderSide, TimeInForce, OrderClass, OptionOrderType
 
 # Load secrets from environment for github actions
-ALPACA_API_KEY = os.environ['APCA_API_KEY_ID']
-ALPACA_API_SECRET = os.environ['APCA_API_SECRET_KEY']
-client = TradingClient(ALPACA_API_KEY, ALPACA_API_SECRET, paper=True)
+ALPACA_PAPER_API_KEY = os.environ['APCA_PAPER_API_KEY_ID']
+ALPACA_PAPER_API_SECRET = os.environ['APCA_PAPER_API_SECRET_KEY']
+client = TradingClient(ALPACA_PAPER_API_KEY, ALPACA_PAPER_API_SECRET, paper=True)
 
 # 1. Load buy signals from signals.json
 with open('artifacts/data/signals.json', 'r') as f:
