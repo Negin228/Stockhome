8717431e4de0ffeac68c3b6ed5049e2ba7a259f5
Negin228 <99446649+Negin228@users.noreply.github.com>
2025-12-25 18:41:47 -0800

    Fix formatting issue in news summary display

--- scripts/app.js
+++ scripts/app.js
@@ -19,8 +19,7 @@ function renderBuyCard(b) {
       </div>
       <p class="news-summary">
         RSI=${fmt(b.rsi_str)}&nbsp;&nbsp;P/E=${fmt(b.pe_str)}&nbsp;&nbsp;
-        DMA 50=${fmt(b.dma50_str)}&nbsp;&nbsp;DMA 200=${fmt(b.dma200_str)}&
-        nbsp;&nbsp;Market Cap=$${b.market_cap_str || "N/A"}
+        DMA 50=${fmt(b.dma50_str)}&nbsp;&nbsp;DMA 200=${fmt(b.dma200_str)}&nbsp;&nbsp;Market Cap=$${b.market_cap_str || "N/A"}
         <br>Sell a $${fmt(put.strike, 1)} put option expiring ${put.expiration || "N/A"} for $${fmt(put.premium, 2)}
         <br>[𝚫 ${fmt(put.delta_percent, 1)}% + 💎 ${fmt(put.premium_percent, 1)}%] = ${fmt(put.metric_sum, 1)}%
       </p>
