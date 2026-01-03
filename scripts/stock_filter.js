bcd8d6cc8f5b19829e41e65b248c35c1440d0692
Negin228 <99446649+Negin228@users.noreply.github.com>
2026-01-02 16:58:42 -0800

    Update stock_filter.js

--- scripts/stock_filter.js
+++ scripts/stock_filter.js
@@ -2,7 +2,7 @@
 
 let allStocks = [];
 
-fetch('data/signals.json')  // If this fails, use 'signals.json' based on previous fix
+fetch('../data/signals.json')  // If this fails, use 'signals.json' based on previous fix
   .then(response => {
     if (!response.ok) throw new Error("HTTP error " + response.status);
     return response.json();
